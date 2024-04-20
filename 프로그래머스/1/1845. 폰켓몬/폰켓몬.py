5f5cad2d7bc5d7a13ffaf493476d28595e28f6ac
박서영 <lilianapsy@naver.com>
2024-04-21 00:06:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,9 @@
+
+nums=[]
 def solution(nums):
     unique_pokemon = len(set(nums))
     max_pokemon_to_take = len(nums) // 2
     return min(unique_pokemon, max_pokemon_to_take)
 
-# 예시
-nums = [3, 1, 2, 3]
-print(solution(nums))  # 결과는 2
+
+print(solution(nums)) 
